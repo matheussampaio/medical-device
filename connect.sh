72231d299adb7915823a841c5320f6ecafcf94a7
Matheus Sampaio <matheus1401@gmail.com>
2016-03-20 19:15:10 -0500

    chore: add script for remote connection with device

---
+++ connect.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+adb shell ip route > addrs.txt
+
+ip_addrs=$(awk {'if( NF >=9){print $9;}'} addrs.txt)
+
+rm addrs.txt
+
+adb tcpip 5555
+
+adb connect "$ip_addrs:5555"
+
+echo "you can remove the cable now."
+echo "try `$ pidcat com.medicaldevice`"
